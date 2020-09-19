2216cbff7d78b8b0cb441de2cb63ed16ed9959bd
sashankneupane7 <54479683+sashankneupane7@users.noreply.github.com>
2020-09-19 08:55:53 +0545

    Update tic_tac_toe.py
    Small updates to the code to make it more organized and readable.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,134 +1,105 @@
 #prints the instructions for the user to understand the basics of game...
-print("\n"+"The instructions:"+"\n")
-print("This is a game played by two players.")
-print("1"+" | "+"2"+" | "+"3")
-print("4"+" | "+"5"+" | "+"6")
-print("7"+" | "+"8"+" | "+"9")
+print()
+print('Hello and Welcome to our Tic Tac Toe game')
+print('This is the standard notation of the board positions that you might need to use later.')
+
+for j in range(1,9,3):
+    print(str(j),'|',str(j+1),'|',str(j+2))
+print()
 print("Your response will be received as a number from 1 to 9, each number representing the position as shown above."+"\n")
 
-#-----Global variables-----
-game_still_going = True
-board = ["-","-","-","-","-","-","-","-","-"]
-player1 = input("Who wants to play as X?"+"\n")
-player2 = input("\n"+"And who wants to play as O?" + "\n")
-current_player = "X"
-winner = None
-
-#displays the tic tac toe board
-def display_board():
-    print(board[0], " | ", board[1], " | ", board[2])
-    print(board[3], " | ", board[4], " | ", board[5])
-    print(board[6], " | ", board[7], " | ", board[8])
-    return
-
-#runs the program until the user ends the game
 def play_game():
-    global board
-    board = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
-    while game_still_going:
-        display_board()
-        handle_turn()
-        check_if_win()
-        check_if_tie()
-        flip_player()
-    return
+    the_board()
+    position_checker()
+    game_checker()
 
-#handles turns of the user, and checks if the provided input is valid
-def handle_turn():
+def the_board():
+    print()
+    for i in range(0,8,3):
+        print(moves[i],'|',moves[i+1],'|',moves[i+2])
+
+def change_turn():
     global current_player
-    global player1
-    global player2
-    position = None
-    
-    #prints player's name to make the game process be less confusing to players
-    if current_player == "X":
-        position = input("\n"+"It's "+player1+"'s turn. Enter any number from 1 to 9:")
-    elif current_player == "O":
-        position = input("\n"+"It's "+player2+"'s turn. Enter any number from 1 to 9:")
-    
-    #checks whether the provided input above is valid
-    while position not in ["1","2","3","4","5","6","7","8","9"] or board[int(position)-1] != "-":
-        position = input("Invalid input. Enter non-repeated number from only 1 to 9:")
+    if current_player == player1:
+        current_player = player2
+    else:
+        current_player = player1
 
-    board[int(position)-1] = current_player
-    return
+def position_checker():
+    while True:
+        try:
+            play_position = int(input('It is ' + current_player + "'s turn. Enter the position where you want to play. (1-9): "))-1
+        except ValueError:
+            print('You must be kidding, right? Input a number from 1 to 9, or you would just end up messing with this infinite loop. :)')
+            continue
+        if play_position >= 9:
+            print('Let me take a moment to remind you that there are only nine spaces in traditional TicTacToe.')
+            continue
+        elif moves[play_position] == '-':
+            if current_player == player1:
+                moves[play_position] = 'O'
+            else:
+                moves[play_position] = 'X'
+            break
+        else:
+            print('The position is already used up. Please make sure you choose an empty position to continue playing.')
+            continue
 
-#checks if any of two users has already won the game
-def check_if_win():
-    global player1
-    global player2
-    winner_by_row = won_by_row()
-    winner_by_column = won_by_column()
-    winner_by_diagonal = won_by_diagonal()
-    if winner_by_row or winner_by_column or winner_by_diagonal == "X":
-        display_board()
-        print("\n"+player1+" won this game.")
-        once_more()
-    elif winner_by_row or winner_by_column or winner_by_diagonal == "O":
-        display_board()
-        print("\n"+player2+" won this game.")
-        once_more()
-    return
+def game_checker():
+    draw_checker()
+    row_checker()
+    diagonal_checker()
+    column_checker()
 
-#checks if three elements in a single row is same
-def won_by_row():
-    winner_by_row = None
-    if board[0] == board[1] == board[2] != "-":
-        winner_by_row = board[0]
-    elif board[3] == board[4] == board[5] != "-":
-        winner_by_row = board[3]
-    elif board[6] == board[7] == board[8] != "-":
-        winner_by_row = board[7]
-    return(winner_by_row)
+def won_the_game():
+    global winner
+    the_board()
+    winner = current_player
+    print(winner, 'won the game. Thanks for playing with us!')
 
-#checks if three elements in a single column is same
-def won_by_column():
-    winner_by_column = None
-    if board[0] == board[3] == board[6] != "-":
-        winner_by_column = board[0]
-    elif board[1] == board[4] == board[7] != "-":
-        winner_by_column = board[1]
-    elif board[2] == board[5] == board[8] != "-":
-        winner_by_column = board[2]
-    return(winner_by_column)
+def row_checker():
+    if moves[0] == moves[1] == moves[2] != '-' or moves[3] == moves[4] == moves[5] != '-' or moves[6] == moves[7] == moves [8] != '-':
+        won_the_game()
 
-#checks if any of two diagonal's elements are same
-def won_by_diagonal():
-    winner_by_diagonal = None
-    if board[0] == board[4] == board[8] != "-":
-        winner_by_diagonal = board[0]
-    elif board[6] == board[4] == board[2] != "-":
-        winner_by_diagonal = board[6]
-    return(winner_by_diagonal)
+def column_checker():
+    if moves[0] == moves[3] == moves[6] != '-' or moves[1] == moves[4] == moves[7] != '-' or moves[2] == moves[5] == moves[8] != '-':
+        won_the_game()
 
-#checks if the game is tied
-def check_if_tie():
-    if "-" not in board:
-        print("\n"+"It's a tie.")
-        once_more()
-    return
+def diagonal_checker():
+    if moves[0] == moves[4] == moves[8] != '-' or moves[2] == moves[4] == moves[6] != '-':
+        won_the_game()
 
-#flips the player on each move
-def flip_player():
-    global current_player
-    if current_player == "X":
-        current_player = "O"
-    elif current_player == "O":
-        current_player = "X"
-    return
+def draw_checker():
+    global winner
+    if '-' not in moves.values():
+        print('It is a draw. Thanks for playing with us!')
+        winner = 'No one'
 
-#asks user if they want to play again
-def once_more():
-    do_you_wanna_play = input("Do you want to play the game once again? Answer in a yes or no:")
-    while do_you_wanna_play.lower() != "yes" and do_you_wanna_play.lower() != "no":
-        do_you_wanna_play = input("Invalid response. Do you want to play the game again?"+"\n")
-    if do_you_wanna_play.lower() == "yes":
+def player_response():
+    while True:
+        player_interest = input('Do you want to play again? Answer in (yes/no)? ')
+        if player_interest.lower() == 'yes':
+            print('Alright! Here we go again.')
+            return 'yes'
+        elif player_interest.lower() == 'no':
+            print('Thank you again for playing with us. See you again.')
+            return 'no'
+        else:
+            print('Invalid input. Please read the instructions carefully!')
+            continue
+
+while True:
+    moves = {0: '-', 1: '-', 2: '-', 3: '-', 4: '-', 5: '-', 6: '-', 7: '-', 8: '-'}
+    player1 = input('Enter the name of the player who is going to play with O: ')
+    player2 = input('Enter the name of the player who is going to play with X: ')
+    current_player = player1
+    winner = ''
+
+    while winner == '':
         play_game()
-    elif do_you_wanna_play.lower() == "no":
-        print("\n"+"Thank you for playing this game. Have a wonderful day ahead."+"\n")
-        input("Press enter to exit this window.")
-        quit()
-    return
+        change_turn()
 
-#calls the function to start the game
-play_game()
+    if player_response() == 'yes':
+        continue
+    else:
+        break
